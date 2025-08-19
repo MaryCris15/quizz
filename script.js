c151cc161f68cbba62b92637985472e9152acb19
MaryCris15 <148765393+MaryCris15@users.noreply.github.com>
2025-08-19 15:01:34 +0000

    meee

--- script.js
+++ script.js
@@ -31,9 +31,9 @@ const perguntas = [
       },
     ],
   },
-];
+  {
     enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-    alternativas; [
+    alternativas: [
       {
         texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
         afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
@@ -43,6 +43,7 @@ const perguntas = [
         afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
       },
     ],
+  },
   {
     enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
     alternativas: [
@@ -56,8 +57,7 @@ const perguntas = [
       },
     ],
   },
-
-{
+  {
     enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
     alternativas: [
       {
@@ -70,8 +70,9 @@ const perguntas = [
       },
     ],
   },
+];
 
-let atual=0;
+let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
@@ -85,6 +86,7 @@ function mostraPergunta() {
   caixaAlternativas.textContent = "";
   mostraAlternativas();
 }
+
 function mostraAlternativas() {
   for (const alternativa of perguntaAtual.alternativas) {
     const botaoAlternativas = document.createElement("button");
